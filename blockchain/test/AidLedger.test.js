f19ac832e53ec1c21b24e7269f80ccaf3029cb77
mccedddy <cedricmacallan@gmail.com>
2024-06-07 18:37:05 +0800

    Updated test script

--- blockchain/test/AidLedger.test.js
+++ blockchain/test/AidLedger.test.js
@@ -13,31 +13,73 @@ contract("AidLedger", (accounts) => {
     assert.notEqual(address, undefined);
   });
 
+  it("Creates relief goods", async () => {
+    const result = await this.aidLedger.createReliefGood(
+      accounts[0],
+      "Food",
+      "1 kg",
+      "Prepared",
+      accounts[1],
+      accounts[3]
+    );
+    const reliefGoodCount = await this.aidLedger.reliefGoodCount();
+    assert.equal(reliefGoodCount.toNumber(), 1);
+
+    const event = result.logs[0].args;
+    assert.equal(event.id.toNumber(), 1);
+    assert.equal(event.donor, accounts[0]);
+    assert.equal(event.typeOfGood, "Food");
+    assert.equal(event.weight, "1 kg");
+    assert.equal(event.status, "Prepared");
+    assert.equal(event.handler, accounts[1]);
+    assert.equal(event.recipient, accounts[3]);
+  });
+
   it("Lists relief goods", async () => {
+    // Create a relief good for testing
+    await this.aidLedger.createReliefGood(
+      accounts[0],
+      "Clothes",
+      "2 kg",
+      "Prepared",
+      accounts[1],
+      accounts[3]
+    );
+
     const reliefGoodCount = await this.aidLedger.reliefGoodCount();
     const reliefGood = await this.aidLedger.reliefGoods(reliefGoodCount);
     assert.equal(reliefGood.id.toNumber(), reliefGoodCount.toNumber());
-    assert.include(["Ced", "Kyle"], reliefGood.donor);
-    assert.include(["Hotdog", "Egg"], reliefGood.description);
-    assert.include(["Delivered", "In transit"], reliefGood.status);
-    assert.include(["Migs", "Clarence"], reliefGood.recipient);
-    assert.equal(reliefGoodCount.toNumber(), 3);
+    assert.equal(reliefGood.donor, accounts[0]);
+    assert.equal(reliefGood.typeOfGood, "Clothes");
+    assert.equal(reliefGood.weight, "2 kg");
+    assert.equal(reliefGood.status, "Prepared");
+    assert.equal(reliefGood.handler, accounts[1]);
+    assert.equal(reliefGood.recipient, accounts[3]);
   });
 
-  it("Creates relief goods", async () => {
-    const result = await this.aidLedger.createReliefGood(
-      "donor",
-      "description",
-      "status",
-      "recipient"
-    );
-    const reliefGoodCount = await this.aidLedger.reliefGoodCount();
-    assert.equal(reliefGoodCount.toNumber(), 4);
-    const event = result.logs[0].args;
-    assert.equal(event.id.toNumber(), 4);
-    assert.equal(event.donor, "donor");
-    assert.equal(event.description, "description");
-    assert.equal(event.status, "status");
-    assert.equal(event.recipient, "recipient");
+  it("Updates relief good status and handler", async () => {
+    await this.aidLedger.updateReliefGoodHandler(2, "In transit", accounts[2]);
+    const reliefGood = await this.aidLedger.reliefGoods(2);
+    assert.equal(reliefGood.status, "In transit");
+    assert.equal(reliefGood.handler, accounts[2]);
+  });
+
+  it("Updates relief good status and recipient", async () => {
+    await this.aidLedger.updateReliefGoodRecipient(2, "Delivered", accounts[4]);
+    const reliefGood = await this.aidLedger.reliefGoods(2);
+    assert.equal(reliefGood.status, "Delivered");
+    assert.equal(reliefGood.recipient, accounts[4]);
+  });
+
+  it("Gets relief good details", async () => {
+    // Retrieve details of a relief good
+    const reliefGood = await this.aidLedger.getReliefGood(2);
+    assert.equal(reliefGood[0].toNumber(), 2);
+    assert.equal(reliefGood[1], accounts[0]);
+    assert.equal(reliefGood[2], "Clothes");
+    assert.equal(reliefGood[3], "2 kg");
+    assert.equal(reliefGood[4], "Delivered");
+    assert.equal(reliefGood[5], accounts[2]);
+    assert.equal(reliefGood[6], accounts[4]);
   });
 });
